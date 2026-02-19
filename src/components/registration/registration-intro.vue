c0a140f478e8b008e2ddde4b1686ecfb42ced8fe
nessornot <nessornot404@gmail.com>
2026-02-19 17:59:32 +0300

    feat: Intro registration view

---
+++ src/components/registration/registration-intro.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="title">
+      hse tracker
+    </div>
+    <img src="/img/intro_registration.png" alt="">
+    <p class="info">
+      Добро пожаловать <br> в человекоцентричный трекер <br> успеваемости hse tracker
+    </p>
+    <button class="continue">
+      Регистрация
+    </button>
+    <button class="signin">
+      У меня есть аккаунт. Войти
+    </button>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper{
+    height: 52rem;
+
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .title{
+    font-size: 1rem;
+  }
+
+  .continue{
+    width: 11.3rem;
+    height: 3.7rem;
+    color: #fff;
+    border-radius: 2rem;
+    background-color: #ff6000;
+    border: #fff 0 solid;
+    text-align: center;
+  }
+
+  .info{
+    text-align: center;
+  }
+</style>
